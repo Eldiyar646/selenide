d53e9eee1ee6ca073577f8330168645c9dc195bf
= <=>
2025-09-04 01:17:57 +0600

    Внес еще изменения

--- src/test/java/test/utils/TestRailCaseId.java
+++ src/test/java/test/utils/TestRailCaseId.java
@@ -1,18 +1,18 @@
-//package test.utils;
-//
-//import org.junit.jupiter.api.Tag;
-//import test.Tags;
-//
-//import java.lang.annotation.ElementType;
-//import java.lang.annotation.Retention;
-//import java.lang.annotation.RetentionPolicy;
-//import java.lang.annotation.Target;
-//
-//@Tag(Tags.SMOKE)
-//@Tag(Tags.WEB)
-//
-//@Retention(RetentionPolicy.RUNTIME)
-//@Target({ElementType.METHOD})
-//public @interface TestRailCaseId {
-//    String value();
-//}
+package test.utils;
+
+import org.junit.jupiter.api.Tag;
+import test.Tags;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Tag(Tags.SMOKE)
+@Tag(Tags.WEB)
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface TestRailCaseId {
+    String value();
+}
