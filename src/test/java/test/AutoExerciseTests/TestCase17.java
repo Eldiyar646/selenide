c864cfb80f1dfd36b920f3f65f0a2d2278ea1caa
= <=>
2025-09-06 17:30:13 +0600

    Исправил мелкие изъяны в Jenkinsfile

--- src/test/java/test/AutoExerciseTests/TestCase17.java
+++ src/test/java/test/AutoExerciseTests/TestCase17.java
@@ -25,14 +25,14 @@ public class TestCase17 extends BaseTest {
     @DisplayName("TestCase 17: Remove Products From Cart")
     @Severity(SeverityLevel.BLOCKER)
     @Description("""
-            "1. Launch browser
-            "2. Navigate to url 'http://automationexercise.com'
-            "3. Verify that home page is visible successfully
-            "4. Add products to cart
-            "5. Click 'Cart' button
-            "6. Verify that cart page is displayed
-            "7. Click 'X' button corresponding to particular product
-            "8. Verify that product is removed from the cart""")
+            1. Launch browser
+            2. Navigate to url 'http://automationexercise.com'
+            3. Verify that home page is visible successfully
+            4. Add products to cart
+            5. Click 'Cart' button
+            6. Verify that cart page is displayed
+            7. Click 'X' button corresponding to particular product
+            8. Verify that product is removed from the cart""")
     void removeProductsFromCart() {
         var softAssert = new SoftAssertions();
 
